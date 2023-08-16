d04983c900a042f1fa63e8d4499877fda07246c5
iadaria <jadarya@mail.ru>
2023-08-16 15:23:08 +0900

    add ts-node

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,10 +4,23 @@ import { Request, Response } from "express";
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.get("/chapter", async function (req: Request, res: Response) {
   const chapter = await getChapter();
   console.log(chapter);
   res.json({ chapter });
+});
+
+app.get("/tr", async function (req: Request, res: Response) {
+  //console.log(chapter);
+  res.json({ translate: "hi there" });
 });
 
 app.listen(3001);
